50244502941bd6a8e8e058d1be3b3fd494f31de6
giovana <161227020+rhapozita@users.noreply.github.com>
2024-09-27 18:47:46 -0300

    Update Parte 2 - CONSULTAS SIMPLES E CONSULTAS AVANÇADAS.sql

--- Parte 2 - CONSULTAS SIMPLES E CONSULTAS AVANÇADAS.sql
+++ Parte 2 - CONSULTAS SIMPLES E CONSULTAS AVANÇADAS.sql
@@ -1,13 +1,16 @@
+#Questão 3
 SELECT preco as Valor, nome as Produto 
 FROM produtos 
 WHERE preco > 5000 
 ORDER BY preco, nome;
 
+#Questão 4
 SELECT id as id_produto, preco
 FROM produtos
 WHERE preco > (SELECT AVG(preco) FROM produtos)
 ORDER BY id asc;
 
+#Questão 5
 SELECT ca.nome as categoria, AVG(preco) as media
 FROM produtos pr
 INNER JOIN produtos_categorias pc on pr.id = pc.produto_id
